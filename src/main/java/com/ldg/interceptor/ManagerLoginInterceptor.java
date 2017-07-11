a50a4bd7b2591c0f1687608aedafa0b143bf54a2
LiuDongguang <liudongguang@qq.com>
2017-07-11 14:01:14 +0800

    pajax 定制修改

--- src/main/java/com/ldg/interceptor/ManagerLoginInterceptor.java
+++ src/main/java/com/ldg/interceptor/ManagerLoginInterceptor.java
@@ -1,5 +1,8 @@
 package com.ldg.interceptor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ldg.api.util.JsonUtil;
+import com.ldg.api.vo.ResultMsg;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,7 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class ManagerLoginInterceptor implements HandlerInterceptor {
-
+	private  static String nologin_jsonStr;
+    static {
+		ResultMsg rms=new ResultMsg();
+		rms.setErrcode(101);
+		rms.setErrmsg("未登录！");
+        try {
+            nologin_jsonStr= JsonUtil.parseToJson(rms);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
@@ -31,6 +44,9 @@ public class ManagerLoginInterceptor implements HandlerInterceptor {
 			if (request.getHeader("x-requested-with") != null
 					&& request.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) { // 如果是ajax请求响应头会有，x-requested-with
 				response.setHeader("sessionstatus", "timeout");// 在响应头设置session状态
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json; charset=utf-8");
+				response.getWriter().print(nologin_jsonStr);
 			} else {
 				response.sendRedirect(request.getContextPath() + "/index.jsp");
 			}
